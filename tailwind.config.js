15c227bc7b1a04814240d9dd2a1c77c2580c3c9a
M13chand <mannuc359@gmail.com>
2024-07-13 14:23:39 +0545

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'light-green': '#EDFFF5'
+      },
       container: {
+        padding: "2rem",
         center: true,
         screens: {
-          'sm': '640px',
-          'md': '768px',
-          'lg': '1024px',
-          'xl': '1280px',
-          '2xl': '1685px',
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1685px",
         },
       },
     },
     plugins: [],
-  }
-}
+  },
+};
